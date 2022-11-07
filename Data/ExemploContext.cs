36c3b7cefb9a4ab28d0f09a1ed4833183d8ff664
Gabriele Pollak Santos <gabrielepollak@outlook.com>
2022-11-07 09:07:55 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ExemploContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Exemplo.Models;
+
+namespace Exemplo.Data
+{
+    public class ExemploContext : DbContext
+    {
+        public ExemploContext (DbContextOptions<ExemploContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Exemplo.Models.Person> Person { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Exemplo.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
